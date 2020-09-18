a77f47891a1263c5f576e10c41e247c9b4c3a8ec
rdss33 <raphaeldssoares@gmail.com>
2020-09-18 16:39:01 -0300

    Project: START 2
    Problemas no git, fiz cagada e refiz agora corretamente + cena de Start Menu com botões pra galera começar a desenvolver UI e BG

---
+++ Assets/Scripts/StartMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenu : MonoBehaviour
+{
+    public void StartGame()// Function designed to change the scene from the Start Menu to the Zone_01 Scene when called
+    {
+        /////******* ANIMATION IS REQUIRED ******//////
+        SceneManager.LoadSceneAsync("Zone_01", LoadSceneMode.Single);// Load this scene and close all other current scenes
+    }
+    public void ExitGame()// Function designed to close the application upon being called
+    {
+        /////******* ANIMATION IS REQUIRED ******//////
+        Application.Quit();// Closes the application, only works on executable
+    }
+}

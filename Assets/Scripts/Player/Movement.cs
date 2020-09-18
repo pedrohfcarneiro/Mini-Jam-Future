ea16a04a96577ac64fe51328c562b0131c2d6480
rdss33 <raphaeldssoares@gmail.com>
2020-09-18 17:12:35 -0300

    Cena populada
    Populando a cena "Zone_01"

---
+++ Assets/Scripts/Player/Movement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Vector2 Direction;
+    private CharacterController Controller;
+    void Start()
+    {
+        Controller = GetComponent<CharacterController>();// Reference to the component in the player object
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void CharacterMove()
+    {
+
+    }
+}

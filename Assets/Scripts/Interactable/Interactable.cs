110016ce7eb01d8ff9f2ddaee941054f7a9e45e4
rdss33 <raphaeldssoares@gmail.com>
2020-09-18 23:26:56 -0300

    Scripts de interação
    Fiz alguns scripts que vão servir de base para interações no jogo

--- Assets/Scripts/Interactable/Interactable.cs
+++ Assets/Scripts/Interactable/Interactable.cs
@@ -8,11 +8,15 @@ public class Interactable : MonoBehaviour
     [SerializeField] private float InteractableDistance= .5f;// Defines the distance from which the player can interact with this
     private UnityAction NearInteractable, Interacting;
     private GameObject Player;// Player Reference
+    [SerializeField] private Targetable[] Targets;// Target objects reference
     public virtual void Awake()
     {
         Player = Player?? GameObject.FindGameObjectWithTag("Player");// If player is null, assign a player
         NearInteractable += Player.GetComponent<PlayerClass>().NearInteractable;// Adds this to the listener
         Interacting += Player.GetComponent<PlayerClass>().Interacting;// Adds this to the Listener
+        foreach (Targetable Target in Targets)// Go through the list of Targetable objects
+            if (Target != null)// If not null
+                Interacting += Target.ExecuteAction;// Stores the reference to the linked object
     }
     public virtual void DetectPlayer()// Function designed to detect if the player is withing interactable distance from the object
     {
@@ -20,8 +24,9 @@ public class Interactable : MonoBehaviour
         {
             NearInteractable.Invoke();// Calls all functions tied to  being near an interactable object
             if (Input.GetButtonDown("Interact"))// If the player presses the interact button
+            {
                 Interacting.Invoke();// Calls all functions tied to interacting with an interactable object
+            }
         }
     }
-    
 }
---
+++ Assets/Scripts/Interactable/Link.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Link : Targetable
+{
+    public override void ExecuteAction()// This object's action when its interactable counterpart is actuated
+    {
+        transform.parent.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None | RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezeRotation;
+        gameObject.SetActive(false);
+    }
+}

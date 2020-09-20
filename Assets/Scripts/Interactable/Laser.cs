10d58037670e640992a56bc73ca9ed58ca02d22c
rdss33 <raphaeldssoares@gmail.com>
2020-09-19 22:36:05 -0300

    Laser e Collider
    Lasers foram recriados como objetos para terem um collider e foram adicionados aos prefabs em suas partes quebradas

---
+++ Assets/Scripts/Interactable/Laser.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : Targetable
+{
+    private bool Active = false;
+    public override void ExecuteAction()
+    {
+        GetComponent<Renderer>().enabled= !GetComponent<Renderer>().enabled;// Activates the Renderer
+        Active = true;// Now lasers are active
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(Active)
+            UnityEditor.EditorApplication.isPlaying = false;// Editor leaves play mode
+    }
+}
